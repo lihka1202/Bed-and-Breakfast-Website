bea3c3ddf464140f38a44a6786efe4a824a25b94
Akhil MN <mnakhilwork@gmail.com>
2024-03-05 15:17:13 +0800

    chore(test): test how to write structs

--- Basic Go Testing/main.go
+++ Basic Go Testing/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type User struct{
+	FirstName string
+	LastName string
+	PhoneNumber string
+	Age int
+	BirthDate time.Time
+}
 
 func main () {
-	fmt.Println("Hello World")
+	user := User {
+		FirstName: "Lol",
+		LastName: "Finder",
+		PhoneNumber: "9090",
+		Age: 40,
+	}
+
+	fmt.Println(user.FirstName)
 }
