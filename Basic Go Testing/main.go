ede71588a39dcc0f70dace67f32361153fb129ff
lihka1202 <mnakhilwork@gmail.com>
2024-03-10 12:16:17 +0800

    chore(core): change ide over to goland
    - Ide has been changed to golanad due to better integration and testing

--- Basic Go Testing/main.go
+++ Basic Go Testing/main.go
@@ -9,15 +9,16 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-	n, err := fmt.Fprintf(w, "testing")
+		n, err := fmt.Fprintf(w, "testing")
 
-	if(err != nil) {
-		fmt.Println(err)
-	}
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
 	})
 
 	//! Start the server here
 	_ = http.ListenAndServe(":8080", nil)
+
 }
