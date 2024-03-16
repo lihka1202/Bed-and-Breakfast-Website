9e6cb4fafafc20b3bcddd75a431ae246e968c77d
lihka1202 <mnakhilwork@gmail.com>
2024-03-17 00:40:16 +0800

    feat(core): add layout changes
    - Screen is not being opened, bug

--- Basic Go Testing/pkg/render/render.go
+++ Basic Go Testing/pkg/render/render.go
@@ -8,10 +8,11 @@ import (
 
 // RenderTemplate will allow us to render the template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, fileErr := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, fileErr := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.gohtml")
 
 	if fileErr != nil {
 		fmt.Println("The templates are not being read properly, aborting")
+		fmt.Println(fileErr)
 		return
 	}
 	err := parsedTemplate.Execute(w, nil)
