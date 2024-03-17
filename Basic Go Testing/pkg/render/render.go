73a4084084b12e0c76ba67aaf1c33797caa08fd7
lihka1202 <mnakhilwork@gmail.com>
2024-03-17 13:04:23 +0800

    chore(core): implement template caching
    - rewrote the render function, now called rendertest, need to re-implement this

--- Basic Go Testing/pkg/render/render.go
+++ Basic Go Testing/pkg/render/render.go
@@ -3,8 +3,12 @@ package render
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
+
+// Package level variable
+var tc = make(map[string]*template.Template)
 
 // RenderTemplate will allow us to render the template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
@@ -21,4 +25,21 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 		fmt.Println("Error parsing the template: ", err)
 		return
 	}
+}
+
+func RenderTemplateTest(w http.ResponseWriter, t string) {
+
+	// Check if the template even exits in tc
+
+	tmpl, inMap := tc[t]
+
+	//! If present inMap would be set to true
+	if !inMap {
+		//? Create the template
+	} else {
+		//! State that the template is present and being used
+		log.Println("Using a cached template")
+	}
+
+	err := tmpl.Execute(w, nil)
 }
