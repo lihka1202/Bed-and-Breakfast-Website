6b95c7af601547f44ec3b830419283d5941b6b5f
lihka1202 <mnakhilwork@gmail.com>
2024-03-18 18:03:31 +0800

    chore(core): add template caching
    - Template caching added

--- Basic Go Testing/pkg/render/render.go
+++ Basic Go Testing/pkg/render/render.go
@@ -11,23 +11,23 @@ import (
 var tc = make(map[string]*template.Template)
 
 // RenderTemplate will allow us to render the template
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, fileErr := template.ParseFiles("./../../templates/"+tmpl, "./../../templates/base.layout.gohtml")
+//func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
+//	parsedTemplate, fileErr := template.ParseFiles("./../../templates/"+tmpl, "./../../templates/base.layout.gohtml")
+//
+//	if fileErr != nil {
+//		fmt.Println("The templates are not being read properly, aborting")
+//		fmt.Println(fileErr)
+//		return
+//	}
+//	err := parsedTemplate.Execute(w, nil)
+//
+//	if err != nil {
+//		fmt.Println("Error parsing the template: ", err)
+//		return
+//	}
+//}
 
-	if fileErr != nil {
-		fmt.Println("The templates are not being read properly, aborting")
-		fmt.Println(fileErr)
-		return
-	}
-	err := parsedTemplate.Execute(w, nil)
-
-	if err != nil {
-		fmt.Println("Error parsing the template: ", err)
-		return
-	}
-}
-
-func RenderTemplateTest(w http.ResponseWriter, t string) {
+func RenderTemplate(w http.ResponseWriter, t string) {
 
 	// Check if the template even exits in tc
 
@@ -42,4 +42,28 @@ func RenderTemplateTest(w http.ResponseWriter, t string) {
 	}
 
 	err := tmpl.Execute(w, nil)
+
+	if err != nil {
+		fmt.Println("There is an error while reading the template: ", err)
+		return
+	}
+}
+
+func createTemplate(t string) error {
+	templates := []string{
+		fmt.Sprintf("./../../templates/%s", t),
+		"./../../templates/base.layout.gohtml",
+	}
+
+	//! Parse the template and then execute
+	tmpl, err := template.ParseFiles(templates[0], templates[1])
+
+	if err != nil {
+		fmt.Println("Template cannot be parsed due to", err)
+	}
+	//! Store it inside the holder
+	tc[t] = tmpl
+
+	//! No error then return as such
+	return nil
 }
