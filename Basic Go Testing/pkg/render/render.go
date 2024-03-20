3344fd36f7676957510fb8592308888cc362f274
lihka1202 <mnakhilwork@gmail.com>
2024-03-20 12:02:24 +0800

    feat(core): add new templating paradigm
    - Use Glob, ParseGlob to assimilate the template and add it to the cache

--- Basic Go Testing/pkg/render/render.go
+++ Basic Go Testing/pkg/render/render.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
@@ -19,4 +20,50 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	if err != nil {
 		fmt.Println("Error parsing template", err)
 	}
+}
+
+func createTemplateCache() (map[string]*template.Template, error) {
+	//myCache := make(map[string]*template.Template)
+	myCache := map[string]*template.Template{}
+
+	//! Get everything with *.page.gohtml from ./templates
+	pages, err := filepath.Glob("./templates/*.page.gohtml")
+
+	//! Error check
+	if err != nil {
+		fmt.Println("Something's wrong", err)
+		return myCache, err
+	}
+
+	//? look through all the pages in the slice
+	for _, page := range pages {
+		//! Get the name, filepath.Base() just gets the name
+		name := filepath.Base(page)
+
+		//! init the template string, and do this by just placing the normal page in , no templates
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		//? look through the layouts in the file system and get them out
+		layouts, err := filepath.Glob("./templates/*.layout.gohtml")
+		if err != nil {
+			return myCache, err
+		}
+
+		//! go through layout and see if you have found any matches
+		if len(layouts) > 0 {
+			ts, err = template.ParseGlob("./templates/*.layout.gohtml")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+
+	}
+
+	return myCache, nil
+
 }
