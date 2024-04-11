11dcd1fc653027f5baf23ed106f1acd24bb8471a
lihka1202 <mnakhilwork@gmail.com>
2024-04-11 16:38:23 +0800

    chore(template): redo template engine

--- Basic Go Testing/main.go
+++ Basic Go Testing/main.go
@@ -1,25 +1,39 @@
-package Basic_Go_Testing
+package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-const portNumber = "8080"
+const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-
+	renderTemplate(w, "home.page.gohtml")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
+}
 
-}q
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, parseError := template.ParseFiles("./templates/" + tmpl)
+	if parseError != nil {
+		log.Println("Something went wrong when we were trying to read the template")
+	}
+
+	//! Execute this now
+	execError := parsedTemplate.Execute(w, nil)
+
+	if execError != nil {
+		log.Println("There was an error while executing the template")
+	}
+}
 
 func main() {
 	//! Use the handlers and listen to the port
 	http.HandleFunc("/", Home)
-	http.HandleFunc("/", About)
+	http.HandleFunc("/about", About)
 
 	//! Print out progress to the data
 	fmt.Printf("Starting the server on this port: %s", portNumber)
