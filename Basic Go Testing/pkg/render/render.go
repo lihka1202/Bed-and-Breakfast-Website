086c2c07875611ec86fafb5ab8391396c43da89c
lihka1202 <mnakhilwork@gmail.com>
2024-04-14 15:49:30 +0800

    chore(template): automate template engine
    - Use glob for regex mapping
    - Use ParseGlob to create the templates
    - use buf to write to see any erors
    - Use buffer write to the http writer

--- Basic Go Testing/pkg/render/render.go
+++ Basic Go Testing/pkg/render/render.go
@@ -1,66 +1,89 @@
 package render
 
 import (
+	"bytes"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 // ! Make a universal template cache to store parsed templates
 var tc = make(map[string]*template.Template)
 
-func RenderTemplateWorse(w http.ResponseWriter, tmpl string) {
-	//! Note that using "./temaplates" is bad as it doesnt account for the working directory, in this case stick to this
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
-	parsedTemplate, parseError := template.ParseFiles("./../../templates/"+tmpl, "./../../templates/base.layout.gohtml")
-	if parseError != nil {
-		log.Println("Something went wrong when we were trying to read the template")
-		log.Printf("%v", parseError)
+	//! Create a template cache
+	//? Print this for now
+	tc, err := createTemplate()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//! Get the requested templates as needed
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(err)
 	}
 
-	//! Execute this now
-	execError := parsedTemplate.Execute(w, nil)
+	buf := new(bytes.Buffer)
 
-	if execError != nil {
-		log.Println("There was an error while executing the template")
+	err = t.Execute(buf, nil)
+	if err != nil {
+		log.Fatal("Something went wrong while we dealt with the buffer")
+	}
+
+	//! Render the requested template
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
-// RenderTemplate checks if the template is in the map, if not adds it in
-func RenderTemplate(w http.ResponseWriter, t string) {
-	//! Check if template t is really in the map or not
-	//? First argument is the value, second would be true if it exists or false otherwise
-	_, inMap := tc[t]
+// createTemplate
+func createTemplate() (map[string]*template.Template, error) {
 
-	if !inMap {
-		//! Create the map object in here
-		err := createTemplate(t)
+	var myCache = map[string]*template.Template{}
+
+	//! I think as of now the file path might be wrong, let us try and see if this works
+
+	pages, globErr := filepath.Glob("./../../templates/*.page.gohtml")
+	if globErr != nil {
+		return myCache, globErr
+	}
+
+	fmt.Println("Entering glob stage")
+
+	//! Pages exists, so we can make use of it
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		//! Parse this template and attach some name to it
+		ts, err := template.New(name).ParseFiles(page)
 		if err != nil {
-			log.Fatalf("Something is wrong hre")
+			return myCache, err
 		}
 
-	} else {
-		//! We know we using an existing template
-		log.Println("Using cached template")
+		//! Now check if some template files exist
+		matches, layoutErr := filepath.Glob("./../../templates/*.layout.gohtml")
+		if layoutErr != nil {
+			return myCache, layoutErr
+		}
+
+		//! Check if there are any matches, ie more than 1
+		if len(matches) > 0 {
+			//! Need to assign each of these templates to a glob
+			ts, err := ts.ParseGlob("./../../templates/*.layout.gohtml")
+
+			if err != nil {
+				return myCache, err
+			}
+
+			//? this is a little weird, why does this fail to exist outside the scope
+			myCache[name] = ts
+		}
+
 	}
+	return myCache, nil
 
-	//! Execute it
-	execErr := tc[t].Execute(w, nil)
-
-	if execErr != nil {
-		log.Fatal("Not working, there is an issue with execution")
-	}
-}
-
-func createTemplate(t string) error {
-	parsedTemplate, parseError := template.ParseFiles("./../../templates/"+t, "./../../templates/base.layout.gohtml")
-	if parseError != nil {
-		log.Println("Something went wrong when we were trying to read the template")
-		return parseError
-	}
-
-	tc[t] = parsedTemplate
-
-	//! return a  nil error
-	return nil
 }
