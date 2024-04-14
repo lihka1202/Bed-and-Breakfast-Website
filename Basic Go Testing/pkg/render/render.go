50e13a3dc0525e1de3388c936bcb352108b73bca
lihka1202 <mnakhilwork@gmail.com>
2024-04-14 13:17:10 +0800

    chore(template): add univeral template
    - universal template cache added with error checking

--- Basic Go Testing/pkg/render/render.go
+++ Basic Go Testing/pkg/render/render.go
@@ -6,7 +6,10 @@ import (
 	"net/http"
 )
 
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+// ! Make a universal template cache to store parsed templates
+var tc = make(map[string]*template.Template)
+
+func RenderTemplateWorse(w http.ResponseWriter, tmpl string) {
 	//! Note that using "./temaplates" is bad as it doesnt account for the working directory, in this case stick to this
 
 	parsedTemplate, parseError := template.ParseFiles("./../../templates/"+tmpl, "./../../templates/base.layout.gohtml")
@@ -21,4 +24,43 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	if execError != nil {
 		log.Println("There was an error while executing the template")
 	}
+}
+
+// RenderTemplate checks if the template is in the map, if not adds it in
+func RenderTemplate(w http.ResponseWriter, t string) {
+	//! Check if template t is really in the map or not
+	//? First argument is the value, second would be true if it exists or false otherwise
+	_, inMap := tc[t]
+
+	if !inMap {
+		//! Create the map object in here
+		err := createTemplate(t)
+		if err != nil {
+			log.Fatalf("Something is wrong hre")
+		}
+
+	} else {
+		//! We know we using an existing template
+		log.Println("Using cached template")
+	}
+
+	//! Execute it
+	execErr := tc[t].Execute(w, nil)
+
+	if execErr != nil {
+		log.Fatal("Not working, there is an issue with execution")
+	}
+}
+
+func createTemplate(t string) error {
+	parsedTemplate, parseError := template.ParseFiles("./../../templates/"+t, "./../../templates/base.layout.gohtml")
+	if parseError != nil {
+		log.Println("Something went wrong when we were trying to read the template")
+		return parseError
+	}
+
+	tc[t] = parsedTemplate
+
+	//! return a  nil error
+	return nil
 }
