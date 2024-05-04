d7c836a950e6546f49f91920f8f76e45782b76de
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-05-04 14:27:07 +0800

    feat(sessions): add ip indexing using sessions packaging
    + Add IP indexing using sessions packaging
    + Make use of session and http requester to make this happen

--- Basic Go Testing/pkg/handlers/handlers.go
+++ Basic Go Testing/pkg/handlers/handlers.go
@@ -31,11 +31,28 @@ func (rp *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	stringMap := map[string]string{
 		"test": "Hello World",
 	}
+
+	//! Store the remote IP here, IPv4 or IPv6
+	remoteIP := r.RemoteAddr
+
+	// store the remote IP in the session using "remote_ip" as the key
+	rp.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	//! Pass as a pointer to preserve usage and save time
 	render.RenderTemplate(w, "home.page.gohtml", &models.TemplateData{StringMap: stringMap})
 }
 
 // About is the about page handler
 func (rp *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.gohtml", &models.TemplateData{})
+	//! Get the value from remoteIP
+	remoteIP := rp.App.Session.GetString(r.Context(), "remote_ip")
+
+	//! Place this session in the string map
+	stringMap := map[string]string{}
+
+	//! Place the values in the stringMap
+	stringMap["test"] = "Hello World"
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.gohtml", &models.TemplateData{StringMap: stringMap})
 }
